0d2dc2540f83142cdd9afb9fee90c1c7cd950789
365kim <365listener@gmail.com>
2020-03-11 20:25:04 +0900

    20/03/11

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,25 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <limits.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+#endif
+
+# define NL 1
+# define _EOF 0
+# define ERROR -1
+
+int		get_next_line(int fd, char **line);
+
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *c);
+char	*ft_strcpy(char	*dst, char *stc, size_t n);
+char	*ft_strndup(char *s1, size_t size);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
---
+++ get_next_line/get_next_line_utils_bonus.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mihykim <mihykim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/02 16:06:58 by mihykim           #+#    #+#             */
+/*   Updated: 2020/03/11 18:56:34 by mihykim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(char *s, int c)
+{
+	size_t i;
+
+	if (s == 0)
+		return (0);
+	i = 0;
+	if (c == 0)
+		return ((char *)s + ft_strlen(s));
+	while (s[i])
+	{
+		if (s[i] == (unsigned char)c)
+			return ((char *)s + i);
+		i++;
+	}
+	return (0);
+}
+
+size_t	ft_strlen(char *s)
+{
+	size_t i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strcpy(char *dst, char *src, size_t n)
+{
+	size_t i;
+
+	if (dst == 0 && src == 0)
+		return (dst);
+	i = 0;
+	while (i < n)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	return (dst);
+}
+
+char	*ft_strndup(char *s1, size_t size)
+{
+	char	*dup;
+
+	dup = (char *)malloc(sizeof(char) * (size + 1));
+	if (dup == 0)
+		return (0);
+	ft_strcpy(dup, s1, size);
+	dup[size] = 0;
+	return (dup);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*new_s;
+	size_t	len1;
+	size_t	len2;
+
+	if (s1 == 0 && s2 == 0)
+		return (0);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	new_s = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (new_s == 0)
+		return (0);
+	ft_strcpy(new_s, s1, len1);
+	ft_strcpy(new_s + len1, s2, len2);
+	new_s[len1 + len2] = 0;
+	return (new_s);
+}
