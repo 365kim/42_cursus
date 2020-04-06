8a2f0d1fd2f7ad046e1d183158e4b562865c8e52
Mihye Kim <mihykim@c4r1s7.42seoul.kr>
2020-04-07 01:50:54 +0900

    2020. 04. 07. Checked 42norm

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: mihykim <mihykim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/01 13:57:25 by mihykim           #+#    #+#             */
-/*   Updated: 2020/04/03 21:58:25 by mihykim          ###   ########.fr       */
+/*   Updated: 2020/04/07 01:28:36 by mihykim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 /*
 ** - Allocates (with malloc) and returns a new element
 ** - The variable 'content' is initialized with the value of 'content'
-** - The variable 'next' is initialized to Null
+** - The variable 'next' is initialized to NULL
 */
 
-t_list	*ft_lstnew(void	*content)
+t_list	*ft_lstnew(void *content)
 {
 	t_list	*new;
 
-	new = (t_list *)malloc(sizeof(t_list *));
+	new = malloc(sizeof(t_list));
 	if (new == 0)
 		return (0);
 	new->content = content;
--- libft/ft_putchar_fd.c
+++ libft/ft_putchar_fd.c
@@ -6,14 +6,14 @@
 /*   By: mihykim <mihykim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 15:43:11 by mihykim           #+#    #+#             */
-/*   Updated: 2020/02/28 15:50:33 by mihykim          ###   ########.fr       */
+/*   Updated: 2020/04/07 01:49:45 by mihykim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** Outputs the character ’c’ to the given file descriptor.
+** Outputs the character 'c' to the given file descriptor.
 */
 
 void	ft_putchar_fd(char c, int fd)
