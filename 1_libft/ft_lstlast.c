2d4929c933320a535b4b9af362692577a3e6ced9
Mihye Kim <mihykim@c2r1s7.42seoul.kr>
2020-04-07 14:37:27 +0900

    2020. 04. 07. Added protection

--- 1_libft/ft_lstlast.c
+++ 1_libft/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: mihykim <mihykim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/01 14:42:54 by mihykim           #+#    #+#             */
-/*   Updated: 2020/03/02 01:12:19 by mihykim          ###   ########.fr       */
+/*   Updated: 2020/04/07 13:36:20 by mihykim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,13 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
+	if (lst == NULL)
+		return (NULL);
 	while (lst->next)
 		lst = lst->next;
 	return (lst);
 }
+
+/*
+** line 21-22 : Added protection
+*/
