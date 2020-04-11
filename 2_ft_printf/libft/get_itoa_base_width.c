b24247d203d6df80893a555c4389047fbcb9ccff
Mihye Kim <mihykim@c4r1s7.42seoul.kr>
2020-04-12 02:00:06 +0900

    2020. 04. 12. Prevented overflow by casting to long long

--- 2_ft_printf/libft/get_itoa_base_width.c
+++ 2_ft_printf/libft/get_itoa_base_width.c
@@ -6,19 +6,20 @@
 /*   By: mihykim <mihykim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/17 00:38:12 by mihykim           #+#    #+#             */
-/*   Updated: 2020/03/17 00:38:58 by mihykim          ###   ########.fr       */
+/*   Updated: 2020/04/12 01:50:39 by mihykim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		get_itoa_base_width(int n, long size)
+int		get_itoa_base_width(long n, long size)
 {
-	int		i;
-	long	long_n;
+	int			i;
+	long long	long_n;
 
 	i = n < 1 ? 1 : 0;
-	long_n = n < 0 ? -(long)n : (long)n;
+	n = n < 0 ? -n : n;
+	long_n = n < 0 ? -(long long)n : (long long)n;
 	while (long_n != 0)
 	{
 		long_n = long_n / size;
