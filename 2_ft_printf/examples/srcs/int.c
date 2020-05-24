5488a5590b481955c7108764702f7b5467fd4c57
365kim <365listener@gmail.com>
2020-05-24 17:45:58 +0900

    2020. 05. 24. Added examples of printf's behaviors

---
+++ 2_ft_printf/examples/srcs/int.c
@@ -0,0 +1,149 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   int.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mihykim <mihykim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/22 13:46:35 by mihykim           #+#    #+#             */
+/*   Updated: 2020/05/24 17:41:04 by mihykim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stdio.h"
+
+int		main(void)
+{
+	printf("* **************************************************** *\n");
+	printf("*                                                      *\n");
+	printf("*    printf's behavior with conversion specifier %%d    *\n");
+	printf("*                                                      *\n");
+	printf("* **************************************************** *\n\n");
+
+	printf("printf(\"%%d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%d\", -12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%d|\n\t\t", -12345) - 16);
+
+	printf("printf(\"%%3d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%3d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%7d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%7d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%-7d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%-7d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%-d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%-d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%*d\", 7, 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%*d|\n\t\t", 7, 12345) - 16);
+
+	printf("printf(\"%%*d\", -7, 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%*d|\n\t\t", -7, 12345) - 16);
+
+	printf("printf(\"%%.d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%.d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%.0d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%.0d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%.3d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%.3d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%.7d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%.7d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%-.7d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%-.7d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%-9.7d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%-9.7d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%9.7d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%9.7d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%-7.9d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%-7.9d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%-3.7d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%-3.7d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%-7.3d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%-7.3d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%0d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%0d|\n\t\t", 12345) - 16);
+	
+	printf("printf(\"%%07d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%07d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%d\", 0)\n");
+	printf("return : %d\n", printf("\t\tresult : |%d|\n\t\t", 0) - 16);
+
+	printf("printf(\"%%3d\", 0)\n");
+	printf("return : %d\n", printf("\t\tresult : |%3d|\n\t\t", 0) - 16);
+
+	printf("printf(\"%%3.d\", 0)\n");
+	printf("return : %d\n", printf("\t\tresult : |%3.d|\n\t\t", 0) - 16);
+
+	printf("printf(\"%%03d\", 0)\n");
+	printf("return : %d\n", printf("\t\tresult : |%03d|\n\t\t", 0) - 16);
+
+	printf("printf(\"%%.d\", 0)\n");
+	printf("return : %d\n", printf("\t\tresult : |%.d|\n\t\t", 0) - 16);
+
+	printf("printf(\"%%.3d\", 0)\n");
+	printf("return : %d\n", printf("\t\tresult : |%.3d|\n\t\t", 0) - 16);
+
+	printf("printf(\"%% d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |% d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%% d\", -12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |% d|\n\t\t", -12345) - 16);
+
+	printf("printf(\"%% -d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |% -d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%% 3d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |% 3d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%% 7d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |% 7d|\n\t\t", 12345) - 16);
+
+
+	printf("printf(\"%%+d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%+d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%+d\", -12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%+d|\n\t\t", -12345) - 16);
+
+	printf("printf(\"%%+-d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%+-d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%+-9d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%+-9d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%+9d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%+9d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%+09d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%+09d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%+09d\", -12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%+09d|\n\t\t", -12345) - 16);
+
+	printf("printf(\"%%+9.7d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%+9.7d|\n\t\t", 12345) - 16);
+
+	printf("printf(\"%%+09.7d\", 12345)\n");
+	printf("return : %d\n", printf("\t\tresult : |%+09.7d|\n\t\t", 12345) - 16);
+
+
+	printf("printf(\"%%#d\", 0)\n");
+	printf("\t\tundefined behavior warning: flag '#' with 'd'\n");
+
+	printf("\n\n");
+}
