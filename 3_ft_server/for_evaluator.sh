36311dba408488a76edbb46662714ffc65f5df05
365kim <365listener@gmail.com>
2020-08-21 14:33:58 +0900

    Add a shell script for starting evaluation

---
+++ 3_ft_server/for_evaluator.sh
@@ -0,0 +1,22 @@
+### 0. Fulfill prerequisites
+#      - Double check whether Docker Desktop is running.
+#      - Start evaluation from the ft_server directory.
+
+name_of_image="tmp"
+name_of_container="cont"
+
+
+### 1. Build an image from a 'Dockerfile' using the current directory as context
+#      -t, --tag list                Name and optionally a tag in the 'name:tag' format
+
+docker build -t ${name_of_image} .
+
+
+### 2. Run a command in a new container
+#      --name string                Assign a name to the container
+#      --rm      		              Automatically remove the container when it exits
+#      -i, --interactive            Keep STDIN open even if not attached
+#      -t, --tty                    Allocate a pseudo-TTY
+#      -p, --publish list           Publish a container's port(s) to the host
+
+docker run --name ${name_of_container} --rm -it -p 80:80/tcp -p 80:80/udp -p 443:443/tcp -p 443:443/udp ${name_of_image}
